081696bb43aac078f396970fd4112227b46a37f8
frezz23 <racketpacket@gmail.com>
2016-04-16 12:18:53 -0400

    uploading new scripts

---
+++ changelevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changelevel : MonoBehaviour {
+
+	public void changethislevel(){
+		Application.LoadLevel (1);
+
+	}
+
+	}
+
---
+++ pausecontrol.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pausecontrol : MonoBehaviour {
+
+	public GameObject PauseMenu;
+	public bool pause;
+	// Use this for initialization
+	void Start () {
+		PauseMenu.SetActive (false);
+		pause = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKey (KeyCode.Escape)) {
+			if(pause == false){
+				PauseMenu.SetActive(true);
+				pause = true;
+			}
+				else{
+					pause = false;
+					PauseMenu.SetActive(false);
+				}
+		}
+	}
+
+	public void resume(){
+		PauseMenu.SetActive (false);
+		pause = false;
+	}
+
+		public void quit(){
+		Application.LoadLevel (0);
+	
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (10, 10, 50, 50), "click")) {
+			Debug.Log ("you clicked me");
+		}
+	}
+
+
+}
